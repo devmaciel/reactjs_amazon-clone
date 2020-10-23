a711acfc78f0d0b1090ba933526182d31c3efa30
João Pedro Maciel <joomaciiel2@outlook.com>
2020-10-23 18:58:26 -0300

    static header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     // BEM
     <div className="App">
-      <h1>Hello Clever Programmers, let's build the Amazon Store</h1>
-      {/* Header */}
+      <Header />
       {/* Home */}
     </div>
   );
---
+++ src/Header.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import './Header.css';
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+
+function Header() {
+    return (
+        <div className="header">
+            <img 
+                className="header__logo"
+                src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" 
+                alt="logo"
+            />
+
+            <div className="header__search">
+                <input
+                    className="header__searchInput"
+                    type="text"
+                />
+                <SearchIcon className="header__searchIcon" />
+            </div>
+
+            <div className="header">
+                <div className="header__option">
+                    <span className="header__optionLineOn">
+                        Hello Guest
+                    </span>
+                    <span className="header__optionLineTwo">
+                        Sign In
+                    </span>
+                </div>
+
+                <div className="header__option">
+                    <span className="header__optionLineOn">
+                        Returns
+                    </span>
+                    <span className="header__optionLineTwo">
+                        & Orders
+                    </span>
+                </div>
+
+                <div className="header__option">
+                    <span className="header__optionLineOn">
+                        Your
+                    </span>
+                    <span className="header__optionLineTwo">
+                       Prime
+                    </span>
+                </div>
+
+                <div className="header__optionBasket">
+                    <ShoppingBasketIcon />
+                    <span className="header__optionLineTwo header__basketCount">0</span>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Header
