66bafa33d710fd362e9cf23dd5a8c59fd2bf1b67
João Pedro Maciel <joomaciiel2@outlook.com>
2020-10-24 22:45:29 -0300

    checkoutProduct e remove from basket

--- src/reducer.js
+++ src/reducer.js
@@ -16,6 +16,34 @@ const reducer = (state, action) => {
                 basket: [...state.basket, action.item]
             }
         
+        case 'REMOVE_FROM_BASKET':
+            /*
+                Pega o indice atual de qual foi clicado ID
+                coloca state atual em outra var
+                se existir indice, vai cortar o array daquele item que foi selecionado
+                    e criar um novo tipo de array sem aquele item
+                caso não exista mais o id, exception
+                
+            */
+            const index = state.basket.findIndex(
+                (basketItem) => basketItem.id === action.id
+            );
+
+            let newBasket = [...state.basket];
+
+            if (index >= 0) {
+                newBasket.splice(index, 1);
+            } else {
+                console.warn(
+                    `Cant remove product (id: ${action.id}) as its not in basket!`
+                )
+            }
+
+            return {
+                ...state,
+                basket: newBasket
+            }
+        
         default:
             return state;
     }
