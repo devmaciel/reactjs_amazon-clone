3537e2bcfe60df5d503956d79c8416d9eab80d30
João Pedro Maciel <joomaciiel2@outlook.com>
2020-10-26 17:01:44 -0300

    index-example js from cloud function

--- functions/index-example.js
+++ functions/index-example.js
@@ -1,7 +1,7 @@
 // const functions = require('firebase-functions');
 // const express = require('express');
 // const cors = require('cors');
-// const stripe = require('stripe')('YOUR_SECRETKEY_CLIENT');
+// const stripe = require('stripe')('YOUR_CLIENT_SECRETKEY_FROM_STRIPE');
 
 // // - API
 
@@ -15,8 +15,24 @@
 // // - API routes
 // app.get('/', (request, response) => response.status(200).send('hello world'));
 
+// app.post('/payments/create', async (request, response) => {
+//     const total = request.query.total;
+
+//     console.log('Payment Request Received', total);
+
+//     const paymentIntent = await stripe.paymentIntents.create({
+//         amount: total, // subunites of the currency
+//         currency: 'usd' // coin of the country
+//     });
+    
+//     // OK - Created
+//     response.status(201).send({
+//         clientSecret: paymentIntent.client_secret //client_secret are in the paymentIntent const
+//     });
+// })
+
 // // - Listen command
 // exports.api = functions.https.onRequest(app)
 
-// example endpoint
-// http://localhost:5001/challenge-b90f1/us-central1/api
+// // example endpoint
+// // http://localhost:5001/challenge-b90f1/us-central1/api
